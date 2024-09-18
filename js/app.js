f3899cf8efc09667dde3d2f400d3c74eec4517d1
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2024-09-18 08:36:52 +0100

    finalizei a section about it

--- js/app.js
+++ js/app.js
@@ -59,40 +59,6 @@ setInterval(() => {
   myloop1.click();
 }, 10000);
 
-// Animations
-ScrollReveal().reveal(".top_nav", {
-  origin: "bottom",
-  distance: "20px",
-  opacity: 0,
-});
-ScrollReveal().reveal(".nav", {
-  origin: "bottom",
-  distance: "20px",
-  opacity: 0,
-  delay: 100,
-});
-
-ScrollReveal().reveal(".header", {
-  origin: "bottom",
-  distance: "20px",
-  opacity: 0,
-  delay: 200,
-});
-ScrollReveal().reveal(".section", {
-  origin: "bottom",
-  distance: "20px",
-  opacity: 0,
-  duration: 1000,
-  delay: 100,
-});
-ScrollReveal().reveal(".footer", {
-  origin: "bottom",
-  distance: "20px",
-  opacity: 0,
-  duration: 1000,
-  delay: 100,
-});
-
 /**
  * Menu mobile
  */
